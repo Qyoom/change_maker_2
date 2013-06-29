1ecbf4e459af45e112eb0f4c951db010875fc7e9
Richard Walker <richard@aqume.com>
2013-06-29 14:30:45 -0700

    Currency formatting improvements, visual tweeks

--- scripts/change-maker-2.js
+++ scripts/change-maker-2.js
@@ -6,9 +6,15 @@ function processPayment(e) {
 	e.preventDefault();
 	console.log("processPayment TOP");
 	
-	var cost = moneyFormat($("#cost").val());
-	var payment = moneyFormat($("#payment").val());
-	var changeDue = (payment >= cost) ? (Math.round((payment - cost)*100)/100) : 0.00; /* Round float math to two decimal places */
+	var cost = formatCurrencyCalc($("#cost").val());
+	var payment = formatCurrencyCalc($("#payment").val());
+	
+	if(payment < cost) {
+		$('#error').text('Payment is less than the cost.');
+		return;
+	}
+	
+	var changeDue = (payment >= cost) ? (Math.round((payment - cost)*100)/100) : 0.00; // Round floats to two decimal places
 	// Accumulators	
 	var changeBalRemain = changeDue;
 	var changeInDenoms = [];
@@ -18,10 +24,10 @@ function processPayment(e) {
 	$('#drawer input').each(function(index) {
 		var id = $(this).attr("id"); // id <- denomination name
 		var denomQty = parseInt($(this).val());
-		var denomVal = moneyFormat($(this).attr("data-denom"));
+		var denomVal = formatCurrencyCalc($(this).attr("data-denom"));
 		var denomFactor = (Math.floor(changeBalRemain / denomVal));
 		
-		// Factor and accumulate quantities and amounts - basic idea: denomination quantity * denomination value
+		// Factor and accumulate quantities and amounts - basic idea: denomination factor * denomination value
 		if(denomFactor > 0 && denomQty > 0) {
 			denomFactor = (denomFactor <= denomQty) ? denomFactor : denomQty;
 			changeBalRemain = changeBalRemain - (denomVal * denomFactor);
@@ -31,28 +37,38 @@ function processPayment(e) {
 		}
 	});
 	
-	// Message display
+	// UI display
 	console.log("changeDue: " + changeDue + " | changeInDenoms: " + changeInDenoms + 
 		" | changeBalRemain < 0.01 ? " + (changeBalRemain < 0.01) + " | changeAccume: " + changeAccume);
 	$('#changeInDenom').text(stringifyChange(changeInDenoms));
-	$('#chgDue').text("Change due: " + changeDue).show();
-	if(changeBalRemain > 0.01) {
-		$('#error').text("Unable to process transaction--Not enough change");
+	$('#chgDue').text("Change due: " + formatCurrencyPrint(changeDue));
+	if(changeBalRemain >= 0.01) {
+		$('#error').text("Unable to process transaction--Not enough change!");
 	}
 	else {
 		$('#error').text('');	
 	}
+	// Format input fields display with two decimal places
+	$("#cost").val(formatCurrencyPrint(cost));
+	$("#payment").val(formatCurrencyPrint(payment));
 }
 
 // Maintain two decimal place formatting for floats from strings
-function moneyFormat(str) {
+function formatCurrencyCalc(str) {
 	var result = Math.round(parseFloat(str) * 100) / 100;
 	return result;
 }
 
+function formatCurrencyPrint(num) {
+    num = isNaN(num) || num === '' || num === null ? 0.00 : num;
+    return parseFloat(num).toFixed(2);
+}
+
 // Convert collected data to readable message
 function stringifyChange(change) {
-	var result = "Change returned: ";
+	var result = "";
+	if(change.length == 0) return result;
+	else result = result.concat("Change returned: ");
 	var denom = 0, qty = 1;
 	for(var i = 0; i < change.length; i++) {
 		result = result.concat(change[i][denom], ": ", change[i][qty]);
