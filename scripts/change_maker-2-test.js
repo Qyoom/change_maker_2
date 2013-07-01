4809183a4cef21ccc28167e3315bdd97fa37fe6f
Richard Walker <richard@aqume.com>
2013-07-01 07:46:59 -0700

    Test file

---
+++ scripts/change_maker-2-test.js
@@ -0,0 +1,44 @@
+function setTest1() {
+	console.log("setTest1");
+	$('#drawer').find('input').val('');
+}
+
+function test1() {
+	console.log("test1");
+}
+
+function setTest2() {
+	console.log("setTest2");
+}
+
+function test2() {
+	console.log("test2");
+}
+
+function setTest3() {
+	console.log("setTest3");
+}
+
+function test3() {
+	console.log("test3");
+}
+
+function setTest4() {
+	console.log("setTest4");
+}
+
+function test4() {
+	console.log("test4");
+}
+
+function setTest5() {
+	console.log("setTest5");
+}
+
+function test5() {
+	console.log("test5");
+}
+
+function runAll() {
+	console.log("runAll");	
+}
